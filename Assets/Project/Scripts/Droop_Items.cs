9c0d11e97ded0b0c976c9c436f2724208dcf136a
gmontsell <63999534+gmontsell@users.noreply.github.com>
2021-10-22 17:21:08 +0200

    droping items

---
+++ Assets/Project/Scripts/Droop_Items.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Droop_Items : MonoBehaviour
+{
+   [SerializeField] List<GameObject> item =new List<GameObject>();
+    [SerializeField] float prob_drop = 30.0f;
+    private void OnDestroy()
+    {
+        int random = Random.Range(0,item.Count);
+        int prob = 100-Random.Range(0, 100);
+
+        if (prob>prob_drop) {
+            Instantiate(item[random], transform.position, transform.rotation);
+        }
+
+    }
+}
